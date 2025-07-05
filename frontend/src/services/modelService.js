704ce30fc47ff6a06a153e66abeb60ecf922f02c
Dunghq23 <dungha.4work@gmail.com>
2025-07-05 16:56:19 +0700

    feat(service): Add ModelService and ProcessService for model and process management

---
+++ frontend/src/services/modelService.js
@@ -0,0 +1,104 @@
+import fileDownload from "js-file-download";
+import axiosClient from "~/utils/axiosClient";
+
+const API_URL = '/models';
+
+class ModelService {
+    // Lấy toàn bộ đơn hàng
+    async getAllModel() {
+        try {
+            const response = await axiosClient.get(API_URL);
+            return response.data.data;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi lấy model mới';
+            throw new Error(errorMessage);
+        }
+    }
+
+
+    // Lấy toàn bộ sản phẩm của model
+    async getProductByModelId(modelId) {
+        try {
+            const response = await axiosClient.get(`${API_URL}/${modelId}`);
+            return response.data.data;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi lấy danh sách sản phẩm trong đơn hàng';
+            throw new Error(errorMessage);
+        }
+    }
+
+    async createOrder(formData) {
+        try {
+            const response = await axiosClient.post(`${API_URL}/create-from-excel`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data.message;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi tạo đơn hàng';
+            throw new Error(errorMessage);
+        }
+    }
+
+    async downloadTemplate() {
+        try {
+            const response = await axiosClient.get(`${API_URL}/template`, {
+                responseType: "blob", // để nhận file nhị phân
+            });
+
+            // Gợi ý tên file dựa theo header hoặc mặc định
+            const contentDisposition = response.headers["content-disposition"];
+            const fileNameMatch = contentDisposition?.match(/filename="?(.+)"?/);
+            const fileName = fileNameMatch ? fileNameMatch[1] : "new-model-template.xlsx";
+
+            fileDownload(response.data, fileName);
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || "Lỗi khi tải file mẫu";
+            throw new Error(errorMessage);
+        }
+    }
+
+    async updateReceivedInfo(orderItemId, data) {
+        try {
+            console.log("OOOOOO");
+
+            const response = await axiosClient.patch(`${API_URL}/items/${orderItemId}`, data);
+            return response.data.message;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi cập nhật thông tin nhận hàng';
+            throw new Error(errorMessage);
+        }
+    }
+
+    // Tìm kiếm đơn hàng theo mã vật tư
+    async getOrdersByMaterialCode(material_code) {
+        try {
+            const response = await axiosClient.get(`${API_URL}/items/${material_code}`);
+            return response.data.data;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi lấy danh sách đơn hàng';
+            throw new Error(errorMessage);
+        }
+    }
+
+    // Xóa model
+    async deleteModel(modelId) {
+        try {
+            const response = await axiosClient.delete(`${API_URL}/${modelId}`);
+            return response.data.message;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi xóa model';
+            throw new Error(errorMessage);
+        }
+    }
+}
+
+export default new ModelService();
---
+++ frontend/src/services/processService.js
@@ -0,0 +1,57 @@
+import fileDownload from "js-file-download";
+import axiosClient from "~/utils/axiosClient";
+
+const API_URL = '/processes';
+
+class ProcessService {
+    async createProcess(formData) {
+        try {
+            const response = await axiosClient.post(`${API_URL}`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                },
+            });
+            return response.data.message;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi tạo công đoạn';
+            throw new Error(errorMessage);
+        }
+    }
+
+    async fetchProcessByProductId(productId) {
+        try {
+            const response = await axiosClient.get(`${API_URL}`, {
+                params: { productId }, // 👈 Truyền productId dưới dạng query param
+            });
+            return response.data.data;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi lấy danh sách công đoạn';
+            throw new Error(errorMessage);
+        }
+    };
+
+    async updateStage({ id, completionDate, description }) {
+        try {
+            const response = await axiosClient.patch(
+                `${API_URL}/stages/${id}`,
+                null, // không có request body
+                {
+                    params: {
+                        date: completionDate ? completionDate.toISOString().split('T')[0] : null, // "yyyy-MM-dd"
+                        description,
+                    },
+                }
+            );
+            return response.data.message;
+        } catch (error) {
+            const errorMessage =
+                error?.response?.data?.message || error.message || 'Lỗi không xác định khi cập nhật công đoạn';
+            throw new Error(errorMessage);
+        }
+    }
+
+}
+
+export default new ProcessService;
